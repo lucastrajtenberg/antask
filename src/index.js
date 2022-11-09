347ded0e3710107d76f802dde3c2d4cffbde196b
lucastrajtenberg <lucasjtraj@gmail.com>
2022-11-09 18:37:25 -0300

    feat: add db connection, init dotenv

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+const {connectToDatabase} = require('./database')
 //require modules
 const app = require('./app')
-
+const dotenv = require('dotenv')
 //setting up
 async function main() {
+    dotenv.config()
+    await connectToDatabase()
     const port = app.get('port');
     await app.listen(port);
     console.log('Server on port', port);
