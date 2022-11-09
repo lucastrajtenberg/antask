5e59b38ecadb4a52ae7e98ad8e3f4b9a148ed6f1
lucastrajtenberg <lucasjtraj@gmail.com>
2022-11-08 21:42:30 -0300

    feat: include helloworld to schema

--- src/graphql/schemas.js
+++ src/graphql/schemas.js
@@ -1,6 +1,22 @@
+const helloWorld = require('./queries/helloworld')
 const {GraphQLSchema, GraphQLObjectType} = require('graphql')
 const QueryType = new GraphQLObjectType({
     name: 'query',
     description: 'Query type, provides all queries required by the API',
-    fields: {}
+    fields: {
+        helloWorld
+    }
 })
+/*const MutationType = new GraphQLObjectType({
+    name: 'mutation',
+    description: 'Mutation type, provides all mutations required by the API',
+    fields: {
+        
+    }
+})
+*/
+const schema = new GraphQLSchema({
+    query: QueryType,
+  //  mutation: MutationType
+})
+module.exports = schema;
