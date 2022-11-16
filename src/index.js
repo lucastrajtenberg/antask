f69335d4ffc733b63cd254c4f0a80637ea751cd7
lucastrajtenberg <lucasjtraj@gmail.com>
2022-11-16 19:54:34 -0300

    feat: add try catch

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ const dotenv = require('dotenv')
 //setting up
 async function main() {
     dotenv.config()
-    await connectToDatabase()
+    try {
+        await connectToDatabase()   
+    }
+    catch (error){
+        throw new Error(`Error while connecting to database. Error: ${error}`) //concatenar texto solo se puede hacer con comillas invertidas: `texto ${jose}`
+    }
     const port = app.get('port');
     await app.listen(port);
     console.log('Server on port', port);
