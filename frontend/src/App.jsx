411d5fb82e0af0d6790311ed9160ee6a07a631d2
lucastrajtenberg <lucasjtraj@gmail.com>
2023-04-04 21:18:56 -0300

    feat: create App.jsx structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Task from './components/Task'
+import tasks from './utils/tasks'
 
 export default function App() {
   return (
     <div className='app'>
-      <h1>antask</h1>
+      <h1>Hola</h1>
+      <div className='collection-name'> {/*ESTO TIENE QUE SER UN COMPONENTE*/}
+      </div>
+      <ul className='tasks'>
+        {
+          tasks.map(task => {
+            return(
+              <Task name={task.name} createdAt={task.createdAt} completed={task.completed}></Task>
+            )
+          })
+        }
+      </ul>
     </div>
   )
 }
