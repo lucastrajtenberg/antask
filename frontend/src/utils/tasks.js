3efcc2c8ea7b53a04fd382506d0e59ea60a7b9cb
lucastrajtenberg <lucasjtraj@gmail.com>
2023-04-04 21:20:29 -0300

    feat: create tasks.js util

---
+++ frontend/src/utils/tasks.js
@@ -0,0 +1,48 @@
+const tasks = [
+    {
+      id: 0,
+      name: "Finish the essay collaboration",
+      createdAt: "2023-04-04T23:34:40.970Z",
+      completed: false,
+      subtasks: [
+        { id: 0, name: "Send collaboration files to Jerusha", completed: false },
+        { id: 1, name: "Send child porn to Jerusha", completed: true }
+      ]
+    },
+    {
+      id: 1,
+      name: "Finish the essay collaboration",
+      createdAt: "2023-04-04T23:34:40.970Z",
+      completed: false,
+      subtasks: [
+        { id: 0, name: "Send collaboration files to Jerusha", completed: false },
+        { id: 1, name: "Send child porn to Jerusha", completed: true }
+      ]
+    },
+    {
+      id: 2,
+      name: "Read next chapter of the book in Danish",
+      createdAt: "2023-04-04T23:34:40.970Z",
+      completed: false,
+      subtasks: []
+    },
+    {
+      id: 3,
+      name: "Send the finished assigment",
+      createdAt: "2023-04-04T23:34:40.970Z",
+      completed: true,
+      subtasks: [
+        { id: 0, name: "Send collaboration files to Jerusha", completed: false },
+        { id: 1, name: "Send child porn to Jerusha", completed: true }
+      ]
+    },
+    {
+      id: 4,
+      name: "Read english book chapter 5",
+      createdAt: "2023-04-04T23:34:40.970Z",
+      completed: true,
+      subtasks: []
+    },
+  ]
+  
+  export default tasks
